56f39f502f9782c93f935e5d111de84a3c125b77
zenpa1 <zer0zenpa1@gmail.com>
2025-09-10 23:29:35 +0800

    Refactored to use api.js

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -1,11 +1,9 @@
 // src/utils/authUtils.js
-import axios from "axios";
-
-const API_URL = "http://localhost:62708/api";
+import api from "./api";
 
 export const register = async (username, password) => {
   try {
-    const res = await axios.post(`${API_URL}/register`, { username, password });
+    const res = await api.post("/register", { username, password });
     return { success: true, message: res.data.message };
   } catch (err) {
     return { success: false, message: err.response?.data?.error || "Registration failed" };
@@ -14,8 +12,7 @@ export const register = async (username, password) => {
 
 export const login = async (username, password) => {
   try {
-    const res = await axios.post(`${API_URL}/login`, { username, password });
-    // Save JWT token to localStorage
+    const res = await api.post("/login", { username, password });
     localStorage.setItem("token", res.data.token);
     return { success: true };
   } catch (err) {
